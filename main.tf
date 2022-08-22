e3845d7f13a1683afcf755b0f1b96769d5320ef5
gorkem.altunay@gmail.com <gorkem.altunay@gmail.com>
2022-08-22 20:02:31 +0300

    first commit

---
+++ main.tf
@@ -0,0 +1,102 @@
+provider "aws" {
+  region = var.region_
+  access_key = var.AWS_SECRET_ACCESS_KEY
+  secret_key = var.AWS_SECRET_ACCESS_KEY_ID
+}
+
+resource "aws_instance" "web" {
+  ami = var.amazon_linux_arm64_image_id
+  instance_type = var.min_type
+  key_name = var.key_name_aws_gorkem
+  vpc_security_group_ids = [aws_security_group.main.id]
+  subnet_id = var.default_subnet
+
+  tags = {
+    Name = "Default"
+  }
+ 
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+        {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    },
+            {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+}
+/*
+resource "aws_lb_target_group" "alb_target_group" {
+  name = "albtargetgroup"
+  port = var.http_port
+  protocol = var.http_protocol
+  vpc_id = var.vpc_id
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port = var.http_port
+  protocol = var.http_protocol
+  
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
+
+resource "aws_lb" "alb" {
+  name = "alb"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.main.id]
+  subnets = [ var.default_subnet, var.default_subnet2 ]
+}
+
+resource "aws_lb_target_group_attachment" "attachment" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id = aws_instance.web.id
+  port = var.http_port
+}
+*/
---
+++ variables.tf
@@ -0,0 +1,45 @@
+//ajksdfa
+variable "amazon_linux_arm64_image_id" {
+  type = string
+  default = "ami-090fa75af13c156b4"
+}
+
+variable "min_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "default_subnet" {
+  type = string
+  default = "subnet-0136d595d71110c71"
+}
+
+variable "default_tagging" {
+  type = string
+  default = "IaC EC2"
+}
+
+variable "region_" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "vpc_id" {
+  type = string
+  default = "vpc-02ffed666286ed0c5"
+}
+
+variable "http_port" {
+  type = string
+  default = "80"
+}
+
+variable "http_protocol" {
+  type = string
+  default = "HTTP"
+}
+
+variable "default_subnet2" {
+  type = string
+  default = "subnet-0494b68a0ef498359"
+}
